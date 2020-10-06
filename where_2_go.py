a7de42b359a8676fbb6b9de2a22c6d8b56b0b5a0
jonathanandrade10 <jonathanandrade10@gmail.com>
2020-10-06 22:57:38 +0100

    Removing python function not being used

--- where_2_go.py
+++ where_2_go.py
@@ -1,14 +1,8 @@
 import urllib.request as request
 import json
 
-
-def intersection(lst1, lst2): 
-    return list(set(lst1) & set(lst2)) 
-
 with request.urlopen('https://gist.githubusercontent.com/benjambles/ea36b76bc5d8ff09a51def54f6ebd0cb/raw/ee1d0c16eaf373cccadd3d5604a1e0ea307b2ca0/users.json') as response:
     user_dataset = json.loads(response.read().decode())
-    
-
 
 
 with request.urlopen('https://gist.githubusercontent.com/benjambles/ea36b76bc5d8ff09a51def54f6ebd0cb/raw/ee1d0c16eaf373cccadd3d5604a1e0ea307b2ca0/venues.json') as response:
